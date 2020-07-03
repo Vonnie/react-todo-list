0d50d7b14eab85ae5cf7f294da70210186ffac8f
Vonnie <vonniekinsey@gmail.com>
2020-07-02 21:52:43 -0500

    Updates

---
+++ precache-manifest.3b1a03fa7c0e2f19d9eb55ea131f877e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef23cedb46a4c45545da885e7910a3fa",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "393f726fa1fd0f0a32d3",
+    "url": "/react-todo-list/static/css/main.5350f4d4.chunk.css"
+  },
+  {
+    "revision": "48a9a0c39be15feb9a93",
+    "url": "/react-todo-list/static/js/2.0b23d8c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.0b23d8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393f726fa1fd0f0a32d3",
+    "url": "/react-todo-list/static/js/main.1b6051d8.chunk.js"
+  },
+  {
+    "revision": "345ac7126bf722b6fb11",
+    "url": "/react-todo-list/static/js/runtime-main.1b4aeefe.js"
+  }
+]);
